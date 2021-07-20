bd47f297b3d42f30863390e8f2c008a5e5ffaa48
Khoirony Arief <khoironyarief08@gmail.com>
2021-07-20 20:17:10 +0700

    perombakan

--- liputan69/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-sm-12 mt-3">
-          <p> &copy; Copy Right 2021 PT. Liputan69 Cyber Media (Brengkok Gramedia Digital Group). All rights reserved.</p>
-        </div>
-      </div>
-    </div>
-</footer>
